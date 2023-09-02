8a01c254aaf85d489c0bd09b35063b043afb897e
fever-max <131164588+fever-max@users.noreply.github.com>
2023-09-02 18:45:20 +0900

    test

--- src/server.js
+++ src/server.js
@@ -18,11 +18,8 @@ app.set("views", process.cwd() + "/src/views");
 
 // FFmpeg
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
   next();
 });
 
